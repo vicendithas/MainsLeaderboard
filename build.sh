5e8f46ca0f65ffc559035714d679a5ef1470b6de
rodg <41761494+rodg@users.noreply.github.com>
2025-02-01 17:45:21 -0500

    add file moving

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ wine pip install flask waitress
 wine pyinstaller --noconfirm --onefile --console \
     --icon "Z:\app\static\favicon.ico" \
     --add-data "Z:\app\static;static/"  "Z:\app\server.py"
+
+chmod 777 /app/dist/server.exe
+chmod 777 /app/dist
