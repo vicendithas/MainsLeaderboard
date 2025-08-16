eb80a1cf3cf201be3dc008f4743f915c4d07d17d
sk84uhlivin <sk84uhlivin@gmail.com>
2025-08-16 12:24:04 -0400

    Configured pyinstaller in onedir mode to avoid antivirus detection

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 wine pip install flask waitress 
 
-wine pyinstaller --noconfirm --onefile --console \
+wine pyinstaller --noconfirm --onedir --console \
     --icon "Z:\app\static\favicon.ico" \
     --add-data "Z:\app\static;static/" \
-    --add-data "Z:\app\config.json;config.json" \
     "Z:\app\server.py"
 
 chmod 777 /app/dist/server.exe
